82d9dc7f0d00dda9069323c2c960cee7d51842dd
Teatoller <steven.otieno1@gmail.com>
2020-10-15 08:57:20 +0300

    (project set up): Implement project set up
    - add axios
    - add tailwind
    - add react-icons
    
    [Finishes #001]

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export const future = {
+  // removeDeprecatedGapUtilities: true,
+  // purgeLayersByDefault: true,
+  // defaultLineHeights: true,
+  // standardFontWeights: true
+};
+export const purge = [];
+export const theme = {
+  extend: {}
+};
+export const variants = {};
+export const plugins = [];
